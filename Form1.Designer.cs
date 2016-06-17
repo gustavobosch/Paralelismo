33fe6c6fe9fc6faf8f8bac4ee68202ac7be9965f
gustavobosch <gustavo.bosch@gmail.com>
2016-06-17 09:38:54 -0300

    Interface Criada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Paralelismo {
-    partial class Form1 {
+    partial class NewNotepad {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,12 +23,94 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(638, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mNew,
+            this.mOpen,
+            this.mSave});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(61, 20);
+            this.menuFile.Text = "Arquivo";
+            // 
+            // mOpen
+            // 
+            this.mOpen.Name = "mOpen";
+            this.mOpen.Size = new System.Drawing.Size(152, 22);
+            this.mOpen.Text = "Abrir";
+            // 
+            // mSave
+            // 
+            this.mSave.Name = "mSave";
+            this.mSave.Size = new System.Drawing.Size(152, 22);
+            this.mSave.Text = "Salvar";
+            // 
+            // mNew
+            // 
+            this.mNew.Name = "mNew";
+            this.mNew.Size = new System.Drawing.Size(152, 22);
+            this.mNew.Text = "Novo";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(0, 27);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(638, 466);
+            this.textBox.TabIndex = 1;
+            // 
+            // NewNotepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(638, 492);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "NewNotepad";
+            this.Text = "Novo Notepad";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem mNew;
+        private System.Windows.Forms.ToolStripMenuItem mOpen;
+        private System.Windows.Forms.ToolStripMenuItem mSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Paralelismo {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class NewNotepad : Form {
+        public NewNotepad() {
             InitializeComponent();
         }
     }
